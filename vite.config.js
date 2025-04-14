58395b1dad5f9cacd563c1d7ca7334d3b5ca0524
KyadiggeriN9868 <KyadiggeriN9868@sduhsd.lan>
2025-04-14 09:03:46 -0700

    commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/NikhilK017/vitereactongithub.git',
+  base: 'https://nikhilk017.github.io/vitereactongithub/',
 });
